b768863c308c52b4c97792ea37e0915be375332e
Kamil Deniz <kamil07deniz@gmail.com>
2023-04-02 16:20:08 +0300

    message

---
+++ MemberRegitration.Business/Concrete/MemberManager.cs
@@ -0,0 +1,28 @@
+﻿using MemberRegistration.DataAccess.Abstract;
+using MemberRegistration.Entities.Concrete;
+using MemberRegitration.Business.Abstract;
+using MemberRegitration.Business.ServiceAdapters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberRegitration.Business.Concrete
+{
+    public class MemberManager : IMemberService
+    {
+        private IMemberDal _memberDal;
+        private IKpsService _kpsService;
+        public MemberManager(IMemberDal memberDal,IKpsService kpsService)
+        {
+            _memberDal = memberDal;
+            _kpsService = kpsService;
+        }
+
+        public void Add(Member member)
+        {
+            _memberDal.Add(member);
+        }
+    }
+}
---
+++ MemberRegitration.Business/ServiceAdapters/KpsServiceAdapter.cs
@@ -0,0 +1,20 @@
+﻿using MemberRegistration.Entities.Concrete;
+using MemberRegitration.Business.KpsServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemberRegitration.Business.ServiceAdapters
+{
+    public class KpsServiceAdapter : IKpsService
+    {
+        public bool ValidateUser(Member member)
+        {
+            KPSPublicSoapClient client = new KPSPublicSoapClient();
+           return client.TCKimlikNoDogrula(Convert.ToInt64(member.TcNo),member.FirstName.ToUpper(),
+                member.LastName.ToUpper(),Convert.ToInt32(member.DateOfBird.Year));
+        }
+    }
+}

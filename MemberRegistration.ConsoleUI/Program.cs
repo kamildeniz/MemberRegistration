9f019fcfc292a17d1b1b555f9717dd0bc29a23b1
Kamil Deniz <kamil07deniz@gmail.com>
2023-04-02 23:13:56 +0300

    Message

--- MemberRegistration.ConsoleUI/Program.cs
+++ MemberRegistration.ConsoleUI/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MemberRegistration.Entities.Concrete;
+using MemberRegitration.Business.Abstract;
+using MemberRegitration.Business.DeppendencyResolvers.Ninject;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +13,10 @@ namespace MemberRegistration.ConsoleUI
     {
         static void Main(string[] args)
         {
+            IMemberService memberService = InstanceFactory.GetInstance<IMemberService>();
+            memberService.Add(new Member { Email="kamil07deniz@gmail.com",FirstName="Kamil",LastName="Deniz",DateOfBird=new DateTime(1995,03,24),TcNo="52042079728" });
+            Console.WriteLine("Eklendi");
+            Console.ReadLine();
         }
     }
 }
--- MemberRegitration.Business/DeppendencyResolvers/Ninject/InstanceFactory.cs
+++ MemberRegitration.Business/DeppendencyResolvers/Ninject/InstanceFactory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Ninject;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace MemberRegitration.Business.DeppendencyResolvers.Ninject
 {
-    internal class InstanceFactory
+    public class InstanceFactory
     {
+        public static T GetInstance<T>()
+        {
+            var kernel = new StandardKernel(new BusinessModule());
+            return kernel.Get<T>();
+        }
     }
 }
